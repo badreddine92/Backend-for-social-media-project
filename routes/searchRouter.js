d7968a53f7d240204cdb3b4c7ce69b58da31d3d7
badreddine guennoun <guennounbadreddine29@gmail.com>
2023-05-07 22:19:13 +0100

    Edit search

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -8,35 +8,19 @@ const _ = require('underscore')
 
 
 
-router.post('/:keywords/:where', verifyToken, async (req, res) => {
-    const keywords = req.params.keywords;
-    const location = req.params.where;
+router.post('/:keyword', verifyToken, async (req, res) => {
+    const keyword = req.params.keyword;
   
-   
-    const validLocations = ["users", "recipes"];
-  
-    try {
-      if (!validLocations.includes(location)) {
-        return res.status(400).send("Invalid location");
-      }
-  
-      switch (location) {
-        case "users":
-          const users = await User.find({ username: keywords });
+     
+       try {
+          const users = await User.find({ username: keyword });
           if (_.isEmpty(users)) {
             return res.status(400).send("No users found");
           } else {
             return res.status(200).json(users);
           }
   
-        case "recipes":
-          const recipes = await Recipe.find({ title: keywords });
-          if (_.isEmpty(recipes)) {
-            return res.status(400).send("No recipes found");
-          } else {
-            return res.status(200).json(recipes);
-          }
-      }
+       
     } catch (err) {
       console.error(err);
       return res.status(500).send("Something went wrong");
